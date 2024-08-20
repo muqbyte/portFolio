13e4d7506e244fc0d80ae6c048768662101d3bad
muqtadir <tadir.muqtadir@gmail.com>
2024-08-20 08:57:21 +0800

    project component

--- components/stack.vue
+++ components/stack.vue
@@ -3,25 +3,30 @@
 </script>
 
 <template>
-    <div class="flex flex-col justify-center items-center" style="border:2px solid">
+    <div class="flex flex-col items-center justify-center" style="border:2px solid">
         <div class="mb-2">
             <h1 class="text-3xl font-bold">Stack skills</h1>
         </div>
 
-        <div class="md:grid grid-cols-3 w-full justify-items-center" style="border:2px solid;" id="secondDiv">
-            <div>
+        <div class="w-full grid-cols-3 md:grid justify-items-center" style="border:2px solid;" id="secondDiv">
+            <div style="border:2px solid" class="flex flex-col items-center justify-center">
                 <div>
                     <h2 class="text-xl font-semibold">Front end stacks</h2>
                 </div>
                <div style="display: flex; justify-content: center;" >
-                    <div style="display: flex; flex-direction:column;justify-content: center;">
+                    <div style="border: 2px solid;" class="flex flex-col items-center justify-center">
+                        <img src="/react.png" alt="Example Image" style="width: 70px; height: auto;"/>
+                        <p>React</p>
+                    </div>
+
+                    <div style="border: 2px solid;" class="flex flex-col items-center justify-center">
                         <img src="/nuxt.png" alt="Example Image" style="width: 70px; height: auto;"/>
                         <p>Nuxt.js</p>
                     </div>
                 </div>
             </div>
 
-            <div class="flex flex-col justify-center items-center">
+            <div class="flex flex-col items-center justify-center">
                 <div>
                     <h2 class="text-xl font-semibold">Front end stacks</h2>
                 </div>
